27e730bfbe80834679ddc53cc005ecba26dc2fdf
Stuart Jauncey <stuart.jauncey@shift.online>
2022-01-10 09:08:30 +0000

    adds tweaks

--- src/utils/dateModifier.js
+++ src/utils/dateModifier.js
@@ -1,11 +1,9 @@
 const dateModifier = date => {
-  let cleanedDate = date;
-  let day = date.substring(8, 10);
-  let month = date.substring(5, 7);
-  let year = date.substring(2, 4);
-  let hour = date.substring(11, 13);
-  let minute = date.substring(14, 16);
-  cleanedDate = `Posted on ${day}-${month}-${year} at ${hour}:${minute}`;
-  return cleanedDate;
+  const day = date.substring(8, 10);
+  const month = date.substring(5, 7);
+  const year = date.substring(2, 4);
+  const hour = date.substring(11, 13);
+  const minute = date.substring(14, 16);
+  return `Posted on ${day}-${month}-${year} at ${hour}:${minute}`;
 };
 export default dateModifier;
